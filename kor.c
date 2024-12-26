7958ec2225a4152f2a16719a89a72e47208147b9
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2024-12-26 01:00:45 +0100

    Update and rename gyak2s.c to kor.c

--- kor.c
+++ kor.c
@@ -21,20 +21,6 @@ A kör sugarát a sugar nevű változóban tároljuk. Azért kell eltárolni, me
 
 #define PI 3.14159265358979323846
 
-int puska() {
-    int num = 42;
-    float pi = 3.14159;
-    char letter = 'A';
-    char str[] = "Hello";
-    printf("Egész szám: %d\n", num);              // Egész szám
-    printf("Lebegőpontos: %.2f\n", pi);          // Két tizedesjegy
-    printf("Karakter: %c\n", letter);            // Karakter
-    printf("Karakterlánc: %s\n", str);           // Szöveg
-    printf("Hexadecimális: %x\n", num);          // Hexadecimális
-    printf("Memóriacím: %p\n", (void*)&num);     // Pointer (cím)
-    printf("%% százalékjel: %%\n");              // Százalékjel
-    return 0;
-}
 
 
 int main(void) {
